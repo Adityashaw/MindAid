31db862a992839ba39c25b489fd6035ea0b8bcfb
Tamzid19597 <Tamzid19597@users.noreply.github.com>
2022-07-17 03:27:07 +0600

    merge problem sort

--- src/main/java/com/example/mindaid/Controller/PaymentController.java
+++ src/main/java/com/example/mindaid/Controller/PaymentController.java
@@ -52,23 +52,6 @@ public class PaymentController {
     public String postBooking(Model model, Payment paymentDto, DoctorsDto doctorsDto){
         schedulingService.savePaymentDetails(paymentDto,doctorsDto);
         model.addAttribute(paymentDto);
-        int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
-        Random random = new Random();
-        StringBuilder buffer = new StringBuilder(targetStringLength);
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int)
-                    (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-        String generatedString = buffer.toString();
-        generatedString="https://meet.jit.si/mindaid"+generatedString;
-        paymentDto.setSessionLink(generatedString);
-        paymentDto.setUserId(temporaryObjectHoldService.getUserDto().getUserId());
-        paymentDto.setScheduleDate(doctorsDto.getSelectedScheduleDay());
-        paymentDto.setScheduleTime(doctorsDto.getSelectedScheduleTime());
-        paymentRepository.save(paymentDto);
         return "paymentConfirm";
     }
 }
