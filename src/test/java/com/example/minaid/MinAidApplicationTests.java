362b0ff42a980b08e78f2308ee025727389d87e1
Aditya Shaw <adityashaw.slds@gmail.com>
2024-06-14 20:07:40 +0530

    fix app run

--- src/test/java/com/example/minaid/MinAidApplicationTests.java
+++ src/test/java/com/example/minaid/MinAidApplicationTests.java
@@ -1,9 +1,9 @@
-package com.example.minaid;
+package com.example.mindaid;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MindAidApplication.class)
 class MinAidApplicationTests {
 
     @Test
